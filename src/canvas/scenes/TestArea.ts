6ba3fe918272d15b9a06e89a393926bb5ec68afb
Vladimir Zaguliaev <oinncodr@outlook.com>
2021-04-02 14:55:29 +0300

    Update lights in TestArea scene

--- src/canvas/scenes/TestArea.ts
+++ src/canvas/scenes/TestArea.ts
@@ -15,18 +15,12 @@ scene.background = new Color(0xA0A0A0);
 scene.fog = new Fog(0xA0A0A0, 100, 1000);
 
 const hemiLight = new HemisphereLight(0xFFFFFF, 0x444444);
-hemiLight.position.set(-3, 10, -10);
+hemiLight.position.set(0, 20, 0);
 scene.add(hemiLight);
 
 const dirLight = new DirectionalLight(0xFFFFFF, 1);
-dirLight.position.set(-3, 10, -10);
+dirLight.position.set(-300, 200, -100);
 dirLight.castShadow = true;
-dirLight.shadow.camera.top = 2;
-dirLight.shadow.camera.bottom = -2;
-dirLight.shadow.camera.left = -2;
-dirLight.shadow.camera.right = 2;
-dirLight.shadow.camera.near = 0.1;
-dirLight.shadow.camera.far = 40;
 scene.add(dirLight);
 
 const cubeGeometry = new BoxGeometry();
