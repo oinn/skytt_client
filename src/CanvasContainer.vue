367cbc1a579ff9a9c53cd89c69ea2b0d7c326e77
Vladimir Zaguliaev <oinncodr@outlook.com>
2021-04-02 14:56:25 +0300

    Update CanvasContainer with new GRenderer and styles

--- src/CanvasContainer.vue
+++ src/CanvasContainer.vue
@@ -4,8 +4,9 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { PerspectiveCamera, WebGLRenderer } from 'three';
+import { PerspectiveCamera } from 'three';
 import TestArea from '@/canvas/scenes/TestArea';
+import GRenderer from '@/classes/GRenderer';
 
 export default defineComponent({
   name: 'CanvasContainer',
@@ -13,20 +14,22 @@ export default defineComponent({
     const scene = TestArea;
     const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    const renderer = new WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    this.$el.appendChild(renderer.domElement);
-
     camera.position.y = 1.7;
     camera.position.z = 5;
 
-    function animate() {
-      requestAnimationFrame(animate);
+    const renderer = new GRenderer(this.$el, scene, camera);
 
-      renderer.render(scene, camera);
-    }
-
-    animate();
+    renderer.animate();
   }
 });
 </script>
+
+<style lang="scss">
+#canvas-container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+</style>
