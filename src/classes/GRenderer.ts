90c860d51def7ce7b4400226b756204d2ba7bed8
Vladimir Zaguliaev <oinncodr@outlook.com>
2021-04-02 14:55:51 +0300

    Add GRenderer class

---
+++ src/classes/GRenderer.ts
@@ -0,0 +1,50 @@
+import {
+  Camera,
+  PCFShadowMap,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer
+} from 'three';
+
+export default class GRenderer extends WebGLRenderer {
+  scene: Scene;
+
+  camera: Camera;
+
+  constructor(element: HTMLElement, scene: Scene, camera: Camera) {
+    super({ antialias: true });
+
+    this.scene = scene;
+    this.camera = camera;
+
+    element.appendChild(this.domElement);
+
+    this.updateSize();
+    window.addEventListener('resize', this.updateSize.bind(this));
+
+    this.setPixelRatio(window.devicePixelRatio);
+    this.shadowMap.enabled = true;
+    this.shadowMap.type = PCFShadowMap;
+  }
+
+  updateSize(): void {
+    const { parentElement } = this.domElement;
+    if (parentElement) {
+      const width = parentElement.clientWidth;
+      const height = parentElement.clientHeight;
+
+      this.setSize(width, height);
+
+      if (this.camera instanceof PerspectiveCamera) {
+        this.camera.aspect = width / height;
+        this.camera.updateProjectionMatrix();
+      }
+    }
+  }
+
+  animate(): void {
+    requestAnimationFrame(this.animate.bind(this));
+
+    this.render(this.scene, this.camera);
+  }
+}
