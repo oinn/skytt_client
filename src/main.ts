961b4b6a6deb0047e781a4cd90b49771f8eb95f4
Vladimir Zaguliaev <oinncodr@outlook.com>
2021-04-07 13:21:42 +0300

    Add Socket.io

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue';
+import VueSocketIOExt from 'vue-socket.io-extended';
+import socket from '@/services/socket';
 import App from './App.vue';
 import './registerServiceWorker';
 import store from './store';
 import './styles/style.scss';
 
-createApp(App).use(store).mount('#app');
+createApp(App)
+  .use(store)
+  .use(VueSocketIOExt, socket)
+  .mount('#app');
